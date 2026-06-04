8c02ceee97d3782de933d3c5c2d1016ae72b0596
minnieming <eunmilee9109.1@gmail.com>
2026-06-04 13:43:42 +0900

    feat: [SP-03] greeting service - controller
    greeting 서비스와 컨트롤러를 만들어서 연결하여 api 호출
    controller <-> service <-> formatter

---
+++ backend/ai-backend/src/main/java/com/sesac/aibackend/service/GreetingService.java
@@ -0,0 +1,18 @@
+package com.sesac.aibackend.service;
+
+import com.sesac.aibackend.util.MessageFormatter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // 필수 객체(생성자 객체)는 넣어라.
+// 의존성 주입 중 생성자를 넣어주는 어노테이션
+public class GreetingService {
+
+    private final MessageFormatter formatter; // 위의 어노테이션으로 생성자 주입을 해준다. 그래서 new 하지 않고 가져와서 쓸 수 있다.
+    // 다른 위치에 있는 클래스를 바로 가져와서 쓸 수 있는게 -> bean에 객체로 등록했기 때문이다.
+
+    public String hello(String name) {
+        return formatter.format(name);
+    }
+}
